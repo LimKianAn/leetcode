768d1b58a4ae7a96d6c6d9a709262479a4e0e6bd
Lim, Kian-An <lim.kian.an@gmx.de>
2021-07-14 22:36:16 +0200

    more

---
+++ go/0019.go
@@ -0,0 +1,24 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	e := &ListNode{Next: head} // extended head
+	l, r := e, e               // left, right
+
+	// r moves rightwards n times
+	for i := 0; i < n; i++ {
+		r = r.Next
+	}
+
+	for r.Next != nil {
+		l = l.Next
+		r = r.Next
+	}
+	l.Next = l.Next.Next
+
+	return e.Next
+}
