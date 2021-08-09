605bc6cf2f4b8969f9f796ee722f55e0490f32fa
Lim, Kian-An <lim.kian.an@gmx.de>
2021-08-09 08:33:32 +0200

    more

---
+++ 0086.go
@@ -0,0 +1,26 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func partition(head *ListNode, x int) *ListNode {
+	h1 := &ListNode{} // h := head
+	h2 := &ListNode{}
+	n1 := h1 // n := node
+	n2 := h2
+	for head != nil {
+		if head.Val < x {
+			n1.Next = head
+			n1 = n1.Next
+		} else {
+			n2.Next = head
+			n2 = n2.Next
+		}
+		head = head.Next
+	}
+	n1.Next = h2.Next
+	n2.Next = nil
+	return h1.Next
+}
---
+++ 0092.go
@@ -0,0 +1,23 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func reverseBetween(head *ListNode, left int, right int) *ListNode {
+	extended := &ListNode{Next: head}
+	pivot := extended
+	for i := 0; i < left-1; i++ {
+		pivot = pivot.Next
+	}
+
+	now := pivot.Next
+	for i := 0; i < right-left; i++ {
+		tmp := now.Next
+		now.Next = now.Next.Next
+		tmp.Next = pivot.Next
+		pivot.Next = tmp
+	}
+	return extended.Next
+}
