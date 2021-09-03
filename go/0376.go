28fcdeb2a4706463da9de56cd83810d65c6a312e
Lim, Kian-An <lim.kian.an@gmx.de>
2021-09-03 07:33:15 +0200

    more

---
+++ go/0376.go
@@ -0,0 +1,31 @@
+package main
+
+import "log"
+
+func wiggleMaxLength(nums []int) int {
+	if l := len(nums); l < 2 {
+		return l
+	}
+
+	l := 1
+	diffMemo := nums[1] - nums[0]
+	if diffMemo != 0 {
+		l = 2
+	}
+
+	for i := 2; i < len(nums); i++ {
+		if diff := nums[i] - nums[i-1]; isAlternating(diffMemo, diff) {
+			l++
+			diffMemo = diff
+		}
+	}
+	return l
+}
+
+func isAlternating(last, now int) bool {
+	return last >= 0 && now < 0 || last <= 0 && now > 0 // `=` is for `0,0,1`
+}
+
+func main() {
+	log.Println(wiggleMaxLength([]int{3, 3, 3, 2, 5}))
+}
