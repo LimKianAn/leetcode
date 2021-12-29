a99fecaf6dab8129e8a219b0b0824c0c0877df37
Lim, Kian-An <lim.kian.an@gmx.de>
2021-12-29 16:00:39 +0100

    0347 Add hands-on heap implementation

--- go/0347.go
+++ go/0347.go
@@ -1,56 +1,105 @@
 package main
 
-import "container/heap"
+import "log"
+
+type Heap []*Element
+
+func NewHeap(ee []*Element) *Heap { // log(n)
+	h := Heap(ee)
+	for i := h.Len()/2 - 1; 0 <= i; i-- { // starts from the second last row
+		h.heapifyDown(i)
+	}
+	return &h
+}
+
+func (h *Heap) Len() int {
+	return len(*h)
+}
+
+func (h *Heap) Peek() *Element {
+	return (*h)[0]
+}
+
+func (h *Heap) Pop() *Element { // removes the tip and re-heapifies
+	tip := h.Peek()
+
+	h.swap(h.Len()-1, 0)
+	(*h) = (*h)[:h.Len()-1]
+	h.heapifyDown(0) // re-heapifies
+
+	return tip
+}
+
+func (h *Heap) Push(e *Element) {
+	(*h) = append((*h), e)
+	h.heapifyUp(h.Len() - 1)
+}
+
+func (h *Heap) heapifyDown(i int) { // theta(log n)
+	leftChild := 2*i + 1
+	rightChild := 2*i + 2
+	if leftChild >= h.Len() { // doesn't exists
+		return
+	}
+
+	child := leftChild
+	if rightChild < h.Len() && h.predicate(rightChild, leftChild) {
+		child = rightChild
+	}
+
+	if h.predicate(i, child) {
+		return
+	}
+	h.swap(i, child)
+	h.heapifyDown(child)
+}
+
+func (h *Heap) heapifyUp(i int) { // theta(log n)
+	if i == 0 {
+		return
+	}
+
+	parent := (i - 1) / 2
+	if h.predicate(parent, i) {
+		return
+	}
+	h.swap(parent, i)
+	h.heapifyUp(parent)
+}
+
+// > for max heap
+// < for min heap
+func (h *Heap) predicate(i, j int) bool {
+	return (*h)[i].count >= (*h)[j].count
+}
+
+func (h *Heap) swap(i, j int) {
+	(*h)[i], (*h)[j] = (*h)[j], (*h)[i]
+}
+
+type Element struct {
+	num, count int
+}
 
 func topKFrequent(nums []int, k int) []int {
-	counts := newCounts(nums)
-	heap.Init(counts)
+	m := map[int]int{}
+	for _, num := range nums {
+		m[num]++
+	}
+
+	ee := make([]*Element, 0, len(m))
+	for k, v := range m {
+		ee = append(ee, &Element{k, v})
+	}
+
+	h := NewHeap(ee)
 	ii := []int{}
-	for len(ii) < k {
-		count := heap.Pop(counts).(count)
-		ii = append(ii, count.key)
+	for i := 0; i < k; i++ {
+		ii = append(ii, h.Pop().num)
 	}
 	return ii
 }
 
-type (
-	count struct {
-		key, count int
-	}
-	counts []count
-)
-
-func newCounts(nums []int) *counts {
-	m := make(map[int]int)
-	for _, num := range nums {
-		m[num]++
-	}
-	cs := make([]count, len(m))
-	for key := range m {
-		cs = append(cs, count{key: key, count: m[key]})
-	}
-	counts := counts(cs)
-	return &counts
-}
-
-func (c *counts) Len() int {
-	return len(*c)
-}
-
-func (c *counts) Less(i, j int) bool {
-	return (*c)[i].count > (*c)[j].count // max heap is needed
-}
-
-func (c *counts) Pop() interface{} {
-	tmp := (*c)[c.Len()-1]
-	*c = (*c)[:c.Len()-1]
-	return tmp
-}
-
-func (c *counts) Push(x interface{}) {
-	*c = append(*c, x.(count))
-}
-
-func (c *counts) Swap(i, j int) {
-	(*c)[i], (*c)[j] = (*c)[j], (*c)[i]
+func main() {
+	log.Print(topKFrequent([]int{1, 1, 1, 2, 2, 2, 3, 3, 3}, 3))
 }
---
+++ go/0347_lib.go
@@ -0,0 +1,56 @@
+package main
+
+import "container/heap"
+
+func topKFrequentLib(nums []int, k int) []int {
+	counts := newCounts(nums)
+	heap.Init(counts)
+	ii := []int{}
+	for len(ii) < k {
+		count := heap.Pop(counts).(count)
+		ii = append(ii, count.key)
+	}
+	return ii
+}
+
+type (
+	count struct {
+		key, count int
+	}
+	counts []count
+)
+
+func newCounts(nums []int) *counts {
+	m := make(map[int]int)
+	for _, num := range nums {
+		m[num]++
+	}
+	cs := make([]count, len(m))
+	for key := range m {
+		cs = append(cs, count{key: key, count: m[key]})
+	}
+	counts := counts(cs)
+	return &counts
+}
+
+func (c *counts) Len() int {
+	return len(*c)
+}
+
+func (c *counts) Less(i, j int) bool {
+	return (*c)[i].count > (*c)[j].count // max heap is needed
+}
+
+func (c *counts) Pop() interface{} {
+	tmp := (*c)[c.Len()-1]
+	*c = (*c)[:c.Len()-1]
+	return tmp
+}
+
+func (c *counts) Push(x interface{}) {
+	*c = append(*c, x.(count))
+}
+
+func (c *counts) Swap(i, j int) {
+	(*c)[i], (*c)[j] = (*c)[j], (*c)[i]
+}
