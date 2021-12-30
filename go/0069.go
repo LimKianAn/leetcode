756a2810ec2e7d9df92aed81c9b59dd2919f2910
Lim, Kian-An <lim.kian.an@gmx.de>
2021-12-30 11:37:02 +0100

    0069 Clean up

--- go/0069.go
+++ go/0069.go
@@ -1,14 +1,5 @@
 package main
 
-// Time: O(log x)
-// func mySqrt(x int) int {
-// 	r := x
-// 	for r*r > x {
-// 		r = (r + x/r) >> 1
-// 	}
-// 	return r
-// }
-
 func mySqrt(x int) int {
 	l, r := 0, x+1
 	for l < r {
