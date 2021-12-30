2723b3ec3be49548731dc970d128dcb858af8db5
Lim, Kian-An <lim.kian.an@gmx.de>
2021-12-30 20:19:21 +0100

    0278 Modify to avoid overflow

--- go/0278_binary_search.go
+++ go/0278_binary_search.go
@@ -1,7 +1,7 @@
 package main
 
 func firstBadVersionBinarySearch(n int) int { // 1-based array works as well
-	l, r := 1, n+1
+	l, r := 1, n // [l,r) r should be n+1, but to avoid overflow we use n. If not found in [1,n-1], return n, which must be the answer.
 	for l < r {
 		m := l + (r-l)>>1
 		if isBadVersion(m) {
