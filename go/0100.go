111504cc5bf1ff8bea04e7671eb7fd3c4db2508d
Lim, Kian-An <lim.kian.an@gmx.de>
2021-12-31 19:00:20 +0100

    0100 Add an alternative

--- go/0100.go
+++ go/0100.go
@@ -1,14 +1,29 @@
 package main
 
 func isSameTree(p *TreeNode, q *TreeNode) bool {
-	if p == nil {
-		return q == nil
+	if p == nil && q == nil {
+		return true
 	}
-	if q == nil {
-		return p == nil
+
+	if p == nil || q == nil {
+		return false
 	}
+
 	if p.Val != q.Val {
 		return false
 	}
 	return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
 }
+
+// func isSameTree(p *TreeNode, q *TreeNode) bool {
+// 	if p == nil {
+// 		return q == nil
+// 	}
+// 	if q == nil {
+// 		return p == nil
+// 	}
+// 	if p.Val != q.Val {
+// 		return false
+// 	}
+// 	return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+// }
