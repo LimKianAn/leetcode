cca83641cb91a07e03dbd83f8017d6414a7bc474
Lim, Kian-An <lim.kian.an@gmx.de>
2022-01-02 18:19:12 +0100

    0062 Follow huahua's method

--- go/0062.go
+++ go/0062.go
@@ -1,24 +1,43 @@
 package main
 
-func uniquePaths(m int, n int) int {
-	a := newArray(m, n)
-	for i := 0; i < m; i++ {
-		for j := 0; j < n; j++ {
-			if i == 0 || j == 0 {
-				// only one unique path along the first row and column
-				a[i][j] = 1
+// func uniquePaths(m int, n int) int { // recursion
+// 	if m <= 0 || n <= 0 {
+// 		return 0
+// 	}
+// 	if m == 1 && n == 1 {
+// 		return 1
+// 	}
+// 	return uniquePaths(m-1, n) + uniquePaths(m, n-1)
+// }
+
+// func uniquePaths(m int, n int) int { // recursion with memo array
+// 	memo := new2DArray(m+1, n+1)
+// 	var f func(m, n int) int
+// 	f = func(m, n int) int {
+// 		if m <= 0 || n <= 0 {
+// 			return 0
+// 		}
+// 		if m == 1 && n == 1 {
+// 			return 1
+// 		}
+// 		if memo[m][n] == 0 {
+// 			memo[m][n] = f(m-1, n) + f(m, n-1)
+// 		}
+// 		return memo[m][n]
+// 	}
+// 	return f(m, n)
+// }
+
+func uniquePaths(m int, n int) int { // dp
+	dp := new2DArray(m+1, n+1)
+	dp[1][1] = 1
+	for i := 1; i <= m; i++ {
+		for j := 1; j <= n; j++ {
+			if i == 1 && j == 1 {
 				continue
 			}
-			a[i][j] = a[i][j-1] + a[i-1][j]
+			dp[i][j] = dp[i][j-1] + dp[i-1][j]
 		}
 	}
-	return a[m-1][n-1]
-}
-
-func newArray(m, n int) [][]int {
-	a := make([][]int, m)
-	for i := range a {
-		a[i] = make([]int, n)
-	}
-	return a
+	return dp[m][n]
 }
