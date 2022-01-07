a8fb7d3471edcccfffbf4d3b2ae38d97ae50a4e5
Lim, Kian-An <lim.kian.an@gmx.de>
2022-01-07 15:31:34 +0100

    0077 Follow huahua's template

--- go/0077.go
+++ go/0077.go
@@ -1,22 +1,24 @@
 package main
 
-func combine(n int, k int) (result [][]int) {
-	var f func(head int, memo []int)
-	f = func(head int, memo []int) {
-		if len(memo) == k {
-			result = append(result, copiedInts(memo))
+// Compare with 0046
+func combine(n int, k int) [][]int { // from huahua
+	ans := [][]int{}
+	tmp := make([]int, 0, k)
+	var dfs func(curr int)
+	dfs = func(curr int) {
+		if len(tmp) == k {
+			copied := make([]int, len(tmp))
+			copy(copied, tmp)
+			ans = append(ans, copied)
 			return
 		}
 
-		for i := head; i <= n; i++ {
-			f(i+1, append(memo, i))
+		for i := curr; i <= n; i++ {
+			tmp = append(tmp, i)
+			dfs(i + 1)
+			tmp = tmp[:len(tmp)-1]
 		}
 	}
-
-
-	return
+	dfs(1)
+	return ans
 }
-
-// func main() {
-// 	combine(5, 2)
-// }
