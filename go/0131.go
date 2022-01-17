7b14daf34c8b0712a1bab320976edd86d6ab01b8
Lim, Kian-An <lim.kian.an@gmx.de>
2022-01-17 13:39:59 +0100

    Compact the code

--- go/0131.go
+++ go/0131.go
@@ -1,45 +1,36 @@
+// 2022.01.17
+
 package main
 
-import "log"
+func partition(s string) [][]string {
+	isPalindrome := func(s string) bool {
+		for i, j := 0, len(s)-1; i < j; {
+			if s[i] != s[j] {
+				return false
+			}
+			i++
+			j--
+		}
+		return true
+	}
 
-func partition(s string) (a [][]string) {
+	ans := [][]string{}
 	var dfs func(int, []string)
-	dfs = func(start int, strings []string) {
-		if start == len(s) { // exceeding the upper bound
-			log.Println(copiedStrings(strings))
-			a = append(a, copiedStrings(strings))
+	dfs = func(start int, ss []string) {
+		if start == len(s) { // exceeds the upper bound
+			cp := make([]string, len(ss))
+			copy(cp, ss)
+			ans = append(ans, cp)
 			return
 		}
 
-		for j := start; j < len(s); j++ {
-			sub := s[start : j+1]
-			if isStrPalindrome(sub) {
-				dfs(j+1, append(strings, sub)) // further evaluating
+		for i := start; i < len(s); i++ {
+			sub := s[start : i+1]
+			if isPalindrome(sub) {
+				dfs(i+1, append(ss, sub)) // further
 			}
 		}
 	}
-
 	dfs(0, []string{})
-	return
+	return ans
 }
-
-func copiedStrings(s []string) []string {
-	tmp := make([]string, len(s))
-	copy(tmp, s)
-	return tmp
-}
-
-func isStrPalindrome(s string) bool {
-	for i, j := 0, len(s)-1; i < j; {
-		if s[i] != s[j] {
-			return false
-		}
-		i++
-		j--
-	}
-	return true
-}
-
-// func main() {
-// 	partition("aab")
-// }
