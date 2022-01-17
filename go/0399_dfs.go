2c2ba440742b74172df9993eee6c4d02c92ba9e5
Lim, Kian-An <lim.kian.an@gmx.de>
2022-01-17 20:05:56 +0100

    Better the code
    Signed-off-by: Lim, Kian-An <lim.kian.an@gmx.de>

--- go/0399_dfs.go
+++ go/0399_dfs.go
@@ -1,39 +1,40 @@
+// 2022.01.17
+
 package main
 
-func calcEquation(equations [][]string, values []float64, queries [][]string) []float64 {
-	graph := map[string]map[string]float64{}
+func calcEquation_d(equations [][]string, values []float64, queries [][]string) []float64 {
+	ratioMap := map[string]map[string]float64{}
 	for i := range equations {
 		a := equations[i][0]
 		b := equations[i][1]
-		if graph[a] == nil {
-			graph[a] = map[string]float64{}
+		if ratioMap[a] == nil {
+			ratioMap[a] = map[string]float64{}
 		}
-		if graph[b] == nil {
-			graph[b] = map[string]float64{}
+		if ratioMap[b] == nil {
+			ratioMap[b] = map[string]float64{}
 		}
-
-		quotient := values[i]
-		graph[a][b] = quotient
-		graph[b][a] = 1 / quotient
+		ratioMap[a][b] = values[i]
+		ratioMap[b][a] = 1 / values[i]
 	}
 
-	var dfs func(x, z string, isVisited map[string]bool) float64
-	dfs = func(x, z string, isVisited map[string]bool) float64 {
+	var dfs func(x, z string, visited map[string]bool) float64
+	dfs = func(x, z string, visited map[string]bool) float64 {
 		if x == z {
 			return 1
 		}
-		isVisited[x] = true // sets the new entry point to true
 
-		for y, quotientXY := range graph[x] {
-			if isVisited[y] {
+		visited[x] = true // sets the new entry point to true
+
+		for y, ratioXY := range ratioMap[x] {
+			if visited[y] {
 				continue // avoids loops
 			}
 
-			quotientYZ := dfs(y, z, isVisited)
-			if quotientYZ == -1 {
+			ratioYZ := dfs(y, z, visited)
+			if ratioYZ == -1 {
 				continue // not viable
 			}
-			return quotientXY * quotientYZ
+			return ratioXY * ratioYZ
 		}
 		return -1
 	}
@@ -42,7 +43,7 @@ func calcEquation(equations [][]string, values []float64, queries [][]string) []
 	for _, query := range queries {
 		a := query[0]
 		b := query[1]
-		if graph[a] == nil || graph[b] == nil {
+		if ratioMap[a] == nil || ratioMap[b] == nil {
 			ans = append(ans, -1)
 			continue // done with the current query
 		}
--- go/0399_union_find.go
+++ go/0399_union_find.go
@@ -1,57 +1,60 @@
+// 2022.01.17
+
 package main
 
-func calcEquationUnionFind(equations [][]string, values []float64, queries [][]string) []float64 {
-	type root struct {
-		id    string
+func calcEquation_u(equations [][]string, values []float64, queries [][]string) []float64 {
+	type ancestor struct {
+		name  string
 		ratio float64
 	}
-	rootMap := map[string]*root{}
+	ancestorMap := map[string]*ancestor{} // a/b = ratioAB; a -> (b, ratioAB)
 
-	var getRoot func(id string) *root
-	getRoot = func(id string) *root {
-		if rootID := rootMap[id].id; rootID != id {
-			newRoot := getRoot(rootID)
-			rootMap[id].id = newRoot.id
-			rootMap[id].ratio *= newRoot.ratio
+	var findRoot func(name string) *ancestor
+	findRoot = func(name string) *ancestor {
+		ancestor := ancestorMap[name]
+		if ancestor.name != name {
+			rt := findRoot(ancestor.name)
+			ancestor.name = rt.name // path compression
+			ancestor.ratio *= rt.ratio
 		}
-		return rootMap[id]
+		return ancestorMap[name]
 	}
 
-	for i, equation := range equations {
-		a := equation[0]
-		b := equation[1]
-		ratio := values[i]
-		if rootMap[a] == nil && rootMap[b] == nil {
-			rootMap[a] = &root{b, ratio} // just chooses one as the root
-			rootMap[b] = &root{b, 1}
-		} else if rootMap[a] == nil {
-			rootMap[a] = &root{b, ratio}
-		} else if rootMap[b] == nil {
-			rootMap[b] = &root{a, 1 / ratio}
+	for i, e := range equations {
+		a := e[0]
+		b := e[1]
+		ratioAB := values[i]
+		if ancestorMap[a] == nil && ancestorMap[b] == nil {
+			ancestorMap[a] = &ancestor{b, ratioAB} // just chooses one as the root
+			ancestorMap[b] = &ancestor{b, 1}
+		} else if ancestorMap[a] == nil {
+			ancestorMap[a] = &ancestor{b, ratioAB}
+		} else if ancestorMap[b] == nil {
+			ancestorMap[b] = &ancestor{a, 1 / ratioAB}
 		} else {
-			rootA := getRoot(a)
-			rootB := getRoot(b)
-			if rootA.id != rootB.id { // two distinct graphs
-				rootMap[rootA.id] = &root{rootB.id, ratio / rootA.ratio * rootB.ratio} // merges rootA into rootB; the ratio path rootA -> a -> b -> rootB
+			rtA := findRoot(a)
+			rtB := findRoot(b)
+			if rtA.name != rtB.name { // two distinct graphs
+				ancestorMap[rtA.name] = &ancestor{rtB.name, 1 / rtA.ratio * ratioAB * rtB.ratio} // merges rootA into rootB; the ratio path rootA -> a -> b -> rootB
 			}
 		}
 	}
 
 	ans := []float64{}
-	for _, query := range queries {
-		a := query[0]
-		b := query[1]
-		if rootMap[a] == nil || rootMap[b] == nil {
+	for _, qr := range queries {
+		a := qr[0]
+		b := qr[1]
+		if ancestorMap[a] == nil || ancestorMap[b] == nil {
 			ans = append(ans, -1)
 			continue
 		}
 
-		rootA := getRoot(a)
-		rootB := getRoot(b)
-		if rootA.id != rootB.id {
+		rtA := findRoot(a) // updates ancestorMap!
+		rtB := findRoot(b)
+		if rtA.name != rtB.name {
 			ans = append(ans, -1)
 		} else {
-			ans = append(ans, rootA.ratio/rootB.ratio) // the ratio path a -> rootA (rootB) -> b
+			ans = append(ans, rtA.ratio/rtB.ratio) // the ratio path a -> rootA (rootB) -> b
 		}
 	}
 	return ans
