fb950e75306134a22158aea3bcc5b9fb431b4352
Lim, Kian-An <lim.kian.an@gmx.de>
2022-01-19 10:05:19 +0100

    Add meta

--- go/0392.go
+++ go/0392.go
@@ -1,20 +1,21 @@
+// 2022.01.19
+
 package main
 
-func isSubsequence(s string, t string) bool {
+func isSubsequence(s string, t string) bool { // s:=sub,t:=total
 	if len(s) == 0 {
 		return true
 	}
 	if len(s) > len(t) {
 		return false
 	}
-	for len(t) > 0 {
+	for ; len(t) > 0; t = t[1:] {
 		if s[0] == t[0] {
 			s = s[1:]
 			if len(s) == 0 {
 				return true
 			}
 		}
-		t = t[1:]
 	}
 	return false
 }
