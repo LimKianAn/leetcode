749a9d3f803ea9e8de7d2ef0e2947d78d7231b0b
Lim, Kian-An <lim.kian.an@gmx.de>
2022-02-23 08:15:32 +0100

    Add different solutions

--- go/0169.go
+++ go/0169.go
@@ -1,17 +1,37 @@
 package main
 
-func majorityElement(nums []int) int {
-	count, candidate := 0, 0
+import "sort"
+
+func majorityElement_bm(nums []int) int { // Boyer-Moore, t O(n), sp O(1)
+	count := 0
+	candidate := 0
 	for _, num := range nums {
 		if count == 0 {
 			candidate = num // restarts
 		}
 
 		if num == candidate {
-			count += 1
+			count++
 		} else {
-			count -= 1
+			count--
 		}
 	}
 	return candidate
+}
+
+func majorityElement_ht(nums []int) int { // hash table, t O(n), sp O(1)
+	n := len(nums)
+	count := map[int]int{}
+	for _, num := range nums {
+		count[num]++
+		if count[num] > n/2 {
+			return num
+		}
+	}
+	return -1
+}
+
+func majorityElement_s(nums []int) int { // sort, t O(nlogn), sp O(1)
+	sort.Ints(nums)
+	return nums[len(nums)/2]
 }
