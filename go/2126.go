10d61e52b0eec66da9f03f1596c3efbb2d21f29b
Lim, Kian-An <lim.kian.an.k8s@gmail.com>
2022-02-28 20:24:38 +0100

    Use `sort.Ints` directly

--- go/2126.go
+++ go/2126.go
@@ -1,30 +1,16 @@
+// 2022.02.25
+
 package main
 
 import "sort"
 
 func asteroidsDestroyed(mass int, asteroids []int) bool {
-	curr := uint64(mass)
-	sort.Sort(sortableAsteroids(asteroids))
+	sort.Ints(asteroids)
 	for _, asasteroid := range asteroids {
-		if curr < uint64(asasteroid) {
+		if mass < asasteroid {
 			return false
 		}
-		curr += uint64(asasteroid)
+		mass += asasteroid
 	}
 	return true
-}
-
-type sortableAsteroids []int
-
-func (s sortableAsteroids) Len() int {
-	return len(s)
-}
-
-func (s sortableAsteroids) Less(i, j int) bool {
-	return s[i] < s[j]
-}
-
-func (s sortableAsteroids) Swap(i, j int) {
-	s[i], s[j] = s[j], s[i]
-	return
 }
