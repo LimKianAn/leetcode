5c11522a004e1c2136919102580f2b85a95bf503
Lim, Kian-An <lim.kian.an.k8s@gmail.com>
2022-03-14 20:21:19 +0100

    Better

--- go/0399_dfs.go
+++ go/0399_dfs.go
@@ -1,20 +1,21 @@
 // 2022.01.17
+// 2022.03.14
 
 package main
 
 func calcEquation_d(equations [][]string, values []float64, queries [][]string) []float64 {
 	ratioMap := map[string]map[string]float64{}
 	for i := range equations {
-		a := equations[i][0]
-		b := equations[i][1]
+		a, b := equations[i][0], equations[i][1]
+		value := values[i]
 		if ratioMap[a] == nil {
 			ratioMap[a] = map[string]float64{}
 		}
 		if ratioMap[b] == nil {
 			ratioMap[b] = map[string]float64{}
 		}
-		ratioMap[a][b] = values[i]
-		ratioMap[b][a] = 1 / values[i]
+		ratioMap[a][b] = value
+		ratioMap[b][a] = 1 / value
 	}
 
 	var dfs func(x, z string, visited map[string]bool) float64
@@ -24,7 +25,6 @@ func calcEquation_d(equations [][]string, values []float64, queries [][]string) 
 		}
 
 		visited[x] = true // sets the new entry point to true
-
 		for y, ratioXY := range ratioMap[x] {
 			if visited[y] {
 				continue // avoids loops
@@ -41,14 +41,12 @@ func calcEquation_d(equations [][]string, values []float64, queries [][]string) 
 
 	ans := []float64{}
 	for _, query := range queries {
-		a := query[0]
-		b := query[1]
+		a, b := query[0], query[1]
 		if ratioMap[a] == nil || ratioMap[b] == nil {
 			ans = append(ans, -1)
-			continue // done with the current query
+		} else {
+			ans = append(ans, dfs(a, b, map[string]bool{}))
 		}
-
-		ans = append(ans, dfs(a, b, map[string]bool{}))
 	}
 	return ans
 }
